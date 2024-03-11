3842c405238a0faa1eebd09b26c52e9ee3f46d43
nachlielEasy <33023571+nachpok@users.noreply.github.com>
2024-03-11 15:20:06 +0200

    add service worker and isIOS check

---
+++ qr-app/service-workers.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "qr-generator-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/src/main.tsx",
+  "/public/QR.png",
+  "/public/192.png",
+  "/public/512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  // Check if the request is for a development file
+  if (event.request.url.includes("/node_modules/.vite/")) {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = ["qr-generator-v1"];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
